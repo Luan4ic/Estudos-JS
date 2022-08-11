43459f67317569c6015e3389b164ff99546ad443
Luan4ic <108698851+Luan4ic@users.noreply.github.com>
2022-08-11 16:56:59 -0300

    Add files via upload

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,16 @@
+console.log("Trabalhando com atribuição de variáveis");
+ 
+const idade = 29;
+let nome = "Ricardo";
+const sobrenome = "Carvalho";
+
+console.log(nome, sobrenome)
+console.log("Meu nome é ${nome} ${sobrenome}");
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto =
+
+nome = nome + sobrenome
+console.log(nome)
